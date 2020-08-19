313708943b4e255cb786e0878821ed88d051dc44
highonweb <mothish07122001@outlook.com>
2020-08-19 22:56:45 +0530

    It's time to go home

--- index.js
+++ index.js
@@ -1,16 +1,96 @@
 const canvas=document.querySelector("#canvas")
 const cont=document.querySelector("#controls")
 ctx=canvas.getContext('2d');
+ctx.lineWidth=2;
 var painting = false
 canvas.width = window.innerWidth*98/100
 canvas.height = window.innerHeight*75/100
+var istext = false
 var isDarkmode=false
-var isPoly = false
-
-function poly() {
-    isPoly=true
-
+var isdraw=false
+var isrect = false
+var iscircle= false
+var rectx,recty
+function text(){
+    canvas.style.cursor = "url('curs/text.png'),auto"
+    ctx.globalCompositeOperation = 'source-over';
+    istext = true
+    isrect=false
+    iscircle=false
+    isdraw=false
+    canvas.addEventListener("click",matext)
+    
 }
+function thick(th) {
+    ctx.lineWidth=parseInt(th)
+}
+function matext(e){
+    if(istext){
+       var textie = prompt("Please enter the text", "Doodles")
+    ctx.font='30px Times New Roman';
+    ctx.fillText(textie, e.clientX, e.clientY); 
+    }
+    
+}
+function rect() {
+    canvas.style.cursor = "url('curs/rect.png'),auto"
+    ctx.globalCompositeOperation = 'source-over';
+    istext = false
+    isrect=true
+    iscircle=false
+    isdraw=false
+    canvas.addEventListener("mouseup",storect)
+    canvas.addEventListener('mousedown',starect);
+}
+function circle(){
+    canvas.style.cursor = "url('curs/circle.png'),auto"
+    ctx.globalCompositeOperation = 'source-over';
+    istext = false
+    iscircle=true
+    isrect=false
+    isdraw=false
+    canvas.addEventListener("mouseup",stocir)
+    canvas.addEventListener('mousedown',stacir);
+}
+function stacir(e) {
+    if(iscircle){
+    lat_mousex = e.clientX
+    lat_mousey = e.clientY
+    }
+}
+
+function stocir(e) {
+    if(iscircle){
+        ctx.beginPath();
+        var a = e.clientX-lat_mousex;
+        var b = e.clientY-lat_mousey;
+        ctx.ellipse(lat_mousex, lat_mousey, a, b, 0, 0, 2*Math.PI);
+        
+        ctx.stroke();
+        }
+}
+
+
+//Mousedown
+
+function starect(e) {
+    if(isrect){
+    last_mousex = e.clientX
+    last_mousey = e.clientY
+    }
+}
+
+function storect(e) {
+    if(isrect){
+        ctx.beginPath();
+        var width = e.clientX-last_mousex;
+        var height = e.clientY-last_mousey;
+        ctx.rect(last_mousex,last_mousey,width,height);
+        
+        ctx.stroke();
+        }
+}
+
 
 function clean(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -18,7 +98,12 @@ function clean(){
         return
 }
 function pencil(){
-    
+    canvas.style.cursor = "url('curs/pencil.png'),auto"
+    ctx.globalCompositeOperation = 'source-over';
+    isrect=false
+    iscircle=false
+    isdraw=true
+    istext = false
     if(isDarkmode){
         color('#fff')
         return
@@ -28,6 +113,12 @@ function pencil(){
     }
 }
 function eraser(){
+    canvas.style.cursor = "url('curs/eraser.png'),auto"
+    ctx.globalCompositeOperation = 'destination-out';
+    isrect=false
+    iscircle=false
+    isdraw=true
+    istext = false
     if(!isDarkmode){
         color('#fff')
         return
@@ -37,10 +128,9 @@ function eraser(){
     }
 }
 function start(){
-   
+    if(isdraw){
         painting=true
-    ctx.beginPath();
-    
+    ctx.beginPath();}
     
 }
 function save(){
@@ -55,8 +145,11 @@ function color(col){
 }
 
 function stopp(){
-        painting=false
-    ctx.closePath();
+    if(isdraw){
+      painting=false
+    ctx.closePath();  
+    }
+        
     }
     
 
@@ -64,7 +157,8 @@ function paint(e) {
     if(!painting){
         return
     }
-    ctx.lineWidth=10;    
+     
+    ctx.lineCap='round';
     ctx.lineTo(e.clientX - 20,e.clientY - 20);
     ctx.stroke()
 }
@@ -84,9 +178,9 @@ if(i==1){
     isDarkmode=true
     }
 }
-/*canvas.addEventListener("mousedown",start)
-canvas.addEventListener("mouseup",stopp)
-canvas.addEventListener("mousemove",paint)*/
+canvas.addEventListener("mousedown",start,false)
+canvas.addEventListener("mouseup",stopp,false)
+canvas.addEventListener("mousemove",paint,false)
 canvas.addEventListener("touchstart",start)
 canvas.addEventListener("touchend",stopp)
 canvas.addEventListener("touchmove",paint)
